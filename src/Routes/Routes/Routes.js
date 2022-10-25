97cec4996ade0c2f9a9930fff0a0667fc8c38108
Rabbe <onikmahmudrabbe@gmail.com>
2022-10-26 03:27:21 +0600

    router added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,12 +1,31 @@
 import { createBrowserRouter } from "react-router-dom"
 import Main from "../../layout/Main"
+import Blog from "../../Pages/Blog/Blog"
+import Courses from "../../Pages/Courses/Courses"
+import Faq from "../../Pages/Faq/Faq"
+import Home from "../../Pages/Home/Home/Home"
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
         children: [
-
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses/:id',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
         ]
     }
 ])
