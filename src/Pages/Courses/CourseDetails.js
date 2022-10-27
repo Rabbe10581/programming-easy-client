98c6ff4b11ead9d3397c43f95f9887bc4923ed0d
Rabbe <onikmahmudrabbe@gmail.com>
2022-10-28 05:48:23 +0600

    Course Details card added

--- src/Pages/Courses/CourseDetails.js
+++ src/Pages/Courses/CourseDetails.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
 
     const data = useLoaderData();
     console.log(data);
+    const { name, Image, details, ques } = data;
 
     return (
-        <div>
-            <h2>Detailsssssss</h2>
-        </div>
+        <Card className='w-50 mx-auto'>
+            <Card.Header className='text-center'>{ques}</Card.Header>
+            <Card.Img variant="top" src={Image} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    {details}
+                </Card.Text>
+                <div className='d-flex justify-content-evenly'>
+                    <Button variant="primary">Get premium access</Button>
+                    <Button variant="primary">Download PDF Now</Button>
+                </div>
+            </Card.Body>
+
+        </Card>
     );
 };
 
