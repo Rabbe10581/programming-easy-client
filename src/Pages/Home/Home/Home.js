cc56aa221833471a84c802a3a04a3ead78be8549
Rabbe <onikmahmudrabbe@gmail.com>
2022-10-28 06:29:10 +0600

    home page edited

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,18 @@
-
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 const Home = () => {
 
     return (
-        <div>
-            <img className='w-50' src="https://leverageedu.com/blog/wp-content/uploads/2021/06/Why-Everyone-Should-Learn-to-Code.png" alt="" />
+        <div className='d-flex justify-content-center align-items-center'>
+            <div className='w-50 ms-lg-4 rounded-4'>
+                <img className='' src="https://i.scdn.co/image/ab67616d0000b273b739f84fcfa450046ae38dd7" alt="" />
+            </div>
+            <div className='w-50 me-lg-4'>
+                <h2>“Success usually comes to those who are too busy looking for it.” — Henry David Thoreau</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus corporis vitae voluptas! At similique quaerat voluptatum dolorem est! Ipsam, option.</p>
+                <Button><Link className='text-decoration-none text-white' to="/courses">Get Started</Link ></Button>
+            </div>
         </div>
     );
 };
