2998eeb5033f1af9386856f8ee6f338f7cb48deb
Rabbe <onikmahmudrabbe@gmail.com>
2022-10-28 16:39:59 +0600

    Footer editted

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -5,14 +5,16 @@ import { FaFacebook, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 const Footer = () => {
     return (
         <footer class="mt-5 pt-3 text-center">
-            <div className='mb-2 fs-4'>
+            <div className='mb-2 fs-5'>
                 <Link className='me-2' to=""><FaFacebook></FaFacebook></Link>
                 <Link className='me-2' to=""><FaLinkedinIn></FaLinkedinIn></Link>
                 <Link to=""><FaInstagram></FaInstagram></Link>
             </div>
-            <p><small>©2023. Programming Easy. All rights reserved. Dhaka, Bangladesh.</small></p>
+            <p><small>©2023. Programming Easy. All rights reserved.</small></p>
         </footer >
     );
 };
 
 export default Footer;
+
+// position-fixed start-0 bottom-0 w-100
